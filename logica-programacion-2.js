10b9f4080b75dc9959db378cfe8c7b083b962bdf
Miguel Castro <macg111104@gmail.com>
2026-04-09 14:13:39 -0600

    Inicializando el repo

---
+++ logica-programacion-2.js
@@ -0,0 +1,7 @@
+/* Crear un programa en Javascript que realice lo siguiente:
+○ Debe solicitar al usuario la temperatura en grados Celsius por prompt o por un input.
+○ Debe convertir la temperatura ingresada de grados Celsius a grados Fahrenheit y Kelvin
+○ Debe imprimir ambos resultados por consola o por el DOM.
+○ Debe ser capaz de identificar si los datos de entrada sean de tipo number, en caso contrario debe mandar
+un mensaje de error y volver a solicitar los datos. */
+
