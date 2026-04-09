dc4ec777c2cdf201ecd4858f338254f076974e87
Miguel Castro <macg111104@gmail.com>
2026-04-09 15:53:28 -0600

    Agregando node modules y resto del programa

--- logica-programacion-2.js
+++ logica-programacion-2.js
@@ -5,6 +5,24 @@
 ○ Debe ser capaz de identificar si los datos de entrada sean de tipo number, en caso contrario debe mandar
 un mensaje de error y volver a solicitar los datos. */
 
-//
-//
-//
+console.log("Bienvenidx a mi conversor de temperaturas en java script. \nIntroduce una temperatura en grados Celcius para convertirla a grados Farenheit y Kelvin");
+
+const prompt = require('prompt-sync')();
+let tempC = prompt("\nEscribe aqui la temperatura en grados Celsius que deseas convertir: ");
+tempC = Number(tempC);
+
+//While por si el usuario pone texto en lugar de numeros
+while (isNaN(tempC)) {
+    console.log("\nERROR: Introduce un numero valido, no texto")
+    tempC = Number(prompt("Introduce una temperatura en grados Celcius: "));
+}
+
+console.log("\nGracias, aqui estan los resultados de la conversion: \n")
+
+//Conversion de C a F y K
+
+let tempF = (tempC * 1.8) + 32;
+let tempK = (tempC + 273.15);
+
+console.log(tempC + "° grados Celcius equivalen a " + tempF + "° grados Farenheit");
+console.log(tempC + "° grados Celcius equivalen a " + tempK + "° grados Kelvin")
